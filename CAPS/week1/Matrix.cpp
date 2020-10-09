b3244654642aae4073996f51e3bc3adf38daca67
j1mmo <36548626+j1mmo@users.noreply.github.com>
2020-10-09 17:10:27 +0100

    Update Matrix.cpp

--- CAPS/week1/Matrix.cpp
+++ CAPS/week1/Matrix.cpp
@@ -1,65 +1,108 @@
-#include <vector>
 #include <iostream>
 #include <assert.h>
-#include <memory>
 
 struct Matrix
 {
-    Matrix(const int col, const int row, const std::vector<int>& values = {}) :
-        col(col), row(row)
+    Matrix(int row,int col) :
+	_row(row), _col(col)
     {
-        const unsigned int grid_size = row * col;
-        table = std::make_unique<int*>(new int[grid_size]);
+	alloc();
+    }
+    
+    Matrix(const Matrix& copy) :
+	_row(copy._row), _col(copy._col)
+    {
+	alloc();
+	for(int rows = 0; rows < _row; rows++){
+	    for(int cols = 0; cols < _col; cols++){
+		table[rows][cols] = copy.table[rows][cols];
+	    }
+	}
+    }
+    
+    Matrix& operator=(const Matrix& rhs){
+	if(this == &rhs)
+	    return *this;
 
-        for (unsigned int index = 0; index < grid_size; ++index)
-        {
-            if (values.size() > index)
-                (*table)[index] = values[index];
-            else
-                (*table)[index] = 0;
-        }
+	if(_row != rhs._row || _col != rhs._col){
+	    for(size_t i = 0; i < _row; i++){
+		delete [] table[i];
+	    }
+	    delete[] table;
+	    _row = rhs._row;
+	    _col = rhs._col;
+	    alloc();
+	}
+	for(int rows = 0; rows < _row; rows++){
+	    for(int cols = 0; cols < _col; cols++){
+		table[rows][cols] = rhs.table[rows][cols];
+	    }
+	}
+	return *this;
     }
 
-    const int matrixSize() const {
-        return col * row;
+    void print(){
+	for(int rows = 0; rows < _row; rows++){
+	    for(int cols = 0; cols < _col; cols++){
+		std::cout << table[rows][cols] << " ";
+	    }
+	    std::cout << "\n";
+	}
+	std::cout << "\n";
     }
-
-    void print() const {
-        for (int _row = 0; _row < row; _row++) {
-            std::cout << "\n";
-            for (int _col = 0; _col < col; _col++) {
-                std::cout << " " << (*this)(_col, _row);
-            }
-        }
-        std::cout << "\n";
+    
+    double& operator()(const int row, const int col){
+	return table[row][col];
     }
-
-    int& operator()(const int _col, const int _row) const {
-        return (*table)[_row + row * _col];
+    
+    ~Matrix()
+    {
+	for(size_t i = 0; i < _row; i++){
+	    delete[] table[i];
+	}
+	delete[] table;
     }
-
-    Matrix operator*(const Matrix& other) {
-        assert(other.row == col);
-        Matrix m(row, other.col);
-        for (int _col = 0; _col < m.col; _col++) {
-            for (int _row = 0; _row < m.row; _row++) {
-                for (int traverse = 0; traverse < m.row; traverse++) {
-                    (*m.table)[_row + m.row * _col] += (*table)[_row + traverse * m.row] * (*other.table)[traverse + _col * m.row];
-                }
-            }
-        }
-        return m;
+    Matrix operator*(const Matrix& rhs){
+	assert(_col == rhs._row);
+	Matrix m(_row, rhs._col);
+	for(int rows = 0 ; rows < _row; rows++){
+	    for(int cols = 0; cols < _col; cols++){
+		for(int trav = 0; trav < m._row; trav++){
+		    m(rows,cols) += table[rows][trav] * rhs.table[trav][cols];
+		}
+	    }
+	}
+	return m;
     }
-    int col, row;
-    std::unique_ptr<int*> table;
+    int _row, _col;
+    double** table;
+    
+private:
+    void alloc(){
+	table = new double*[_row];
+	for(size_t cols = 0; cols < _row; cols++){
+	    table[cols] = new double[_col];
+	}
+    }
 };
+
 int main()
 {
-    Matrix m(3, 2, std::vector<int> {1, 2, 3, 4, 5, 6});
-    Matrix n(2, 3, std::vector<int> {1, 2, 3, 4, 5, 6});
-    Matrix multi = m * n;
-    m.print();
-    n.print();
-    multi.print();
+    Matrix m(3,5);
+    for(int i = 0 ; i < 3;i++){
+	for(int j = 0; j < 5; j++){
+	    m(i,j) = j*i+(i+j);
+	}
+    }
+    Matrix n(5,3);
+    for(int i = 0 ; i < 5;i++){
+	for(int j = 0; j < 3; j++){
+	    n(i,j) = j*i+(i+j);
+	}
+    }
+    Matrix p = m * n;
+    //m.print();
+    //n.print();
+    p.print();
     return 0;
 }
