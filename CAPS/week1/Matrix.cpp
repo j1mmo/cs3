48985ef33a9ba48b18c1cef1ee1b6b2db8a9c0f7
j1mmo <36548626+j1mmo@users.noreply.github.com>
2020-10-20 19:51:18 +0100

    Update Matrix.cpp

--- CAPS/week1/Matrix.cpp
+++ CAPS/week1/Matrix.cpp
@@ -1,108 +1,109 @@
+#include <cstddef>
 #include <iostream>
+#include <cstring>
 #include <assert.h>
+#include <thread>
+#include <vector>
 
 struct Matrix
 {
-    Matrix(int row,int col) :
-	_row(row), _col(col)
+    Matrix(const int rows, const int cols) : table(nullptr), _rows(rows), _cols(cols)
     {
-	alloc();
+        table = new float[(rows * cols)];
     }
-    
-    Matrix(const Matrix& copy) :
-	_row(copy._row), _col(copy._col)
+    float& operator()(const int row, const int col) const
     {
-	alloc();
-	for(int rows = 0; rows < _row; rows++){
-	    for(int cols = 0; cols < _col; cols++){
-		table[rows][cols] = copy.table[rows][cols];
-	    }
-	}
+        return table[col + row * _cols];
     }
-    
-    Matrix& operator=(const Matrix& rhs){
-	if(this == &rhs)
-	    return *this;
-
-	if(_row != rhs._row || _col != rhs._col){
-	    for(size_t i = 0; i < _row; i++){
-		delete [] table[i];
-	    }
-	    delete[] table;
-	    _row = rhs._row;
-	    _col = rhs._col;
-	    alloc();
-	}
-	for(int rows = 0; rows < _row; rows++){
-	    for(int cols = 0; cols < _col; cols++){
-		table[rows][cols] = rhs.table[rows][cols];
-	    }
-	}
-	return *this;
-    }
-
-    void print(){
-	for(int rows = 0; rows < _row; rows++){
-	    for(int cols = 0; cols < _col; cols++){
-		std::cout << table[rows][cols] << " ";
-	    }
-	    std::cout << "\n";
-	}
-	std::cout << "\n";
-    }
-    
-    double& operator()(const int row, const int col){
-	return table[row][col];
-    }
-    
-    ~Matrix()
+    Matrix(const Matrix& other)
+        : table(nullptr), _rows(other._rows), _cols(other._cols)
     {
-	for(size_t i = 0; i < _row; i++){
-	    delete[] table[i];
-	}
-	delete[] table;
+        memory_allocation();
+        const auto capacity = _rows * _cols;
+        for (int i = 0; i < capacity; i++)
+            table[i] = other.table[i];
     }
-    Matrix operator*(const Matrix& rhs){
-	assert(_col == rhs._row);
-	Matrix m(_row, rhs._col);
-	for(int rows = 0 ; rows < _row; rows++){
-	    for(int cols = 0; cols < _col; cols++){
-		for(int trav = 0; trav < m._row; trav++){
-		    m(rows,cols) += table[rows][trav] * rhs.table[trav][cols];
+    Matrix operator*(const Matrix& rhs)
+    {
+	assert(_cols == rhs._rows);
+	auto single_cell_calc = [] (const int current_row,
+				      const int current_col,
+				      const int max,
+				      const Matrix& rhs,
+				      const Matrix& lhs,
+				      Matrix& result){
+		for(int k = 0; k < max; k++){
+            result(current_row,current_col) += (rhs)(current_row, k) + (lhs)(k, current_col);
+        }};
+    std::vector<std::thread> threads;
+	Matrix m(_rows, rhs._cols);
+	for(int i = 0; i < _rows; i++){
+	    for(int j = 0; j < rhs._cols; j++){
+		for(int k = 0; k < _cols; k++){
+		    m(i,j) += (*this)(i,k) * rhs(k,j);
 		}
 	    }
 	}
 	return m;
     }
-    int _row, _col;
-    double** table;
-    
-private:
-    void alloc(){
-	table = new double*[_row];
-	for(size_t cols = 0; cols < _row; cols++){
-	    table[cols] = new double[_col];
-	}
+    Matrix& operator=(const Matrix& rhs)
+    {
+        if(this != &rhs)
+        {
+            if(this->capacity() != rhs.capacity())
+            {
+                if(table){
+                    delete [] table;
+                }
+                _rows = rhs._rows;
+                _cols = rhs._cols;
+                memory_allocation();
+                memcpy(table, rhs.table, sizeof(float) * this->capacity());
+            }
+        }
+        return *this;
+    }
+    const int capacity() const
+    {
+        return _rows * _cols;
+    }
+    ~Matrix()
+    {
+        if(table)
+            delete [] table;
+    }
+    float* table;
+    int _rows, _cols;
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
+    private:
+    void memory_allocation()
+    {
+        table = new float[_rows * _cols];
     }
 };
-
+std::ostream& operator<<(std::ostream& os, const Matrix& m)
+{
+    for(int row = 0; row < m._rows; row++){
+        for(int col = 0; col < m._cols; col++){
+            os << m(row,col) << " ";
+        }
+        os << "\n";
+    }
+    os << "\n";
+    return os; 
+}
 int main()
 {
-    Matrix m(3,5);
-    for(int i = 0 ; i < 3;i++){
-	for(int j = 0; j < 5; j++){
-	    m(i,j) = j*i+(i+j);
+    Matrix m(4,4);
+    for(int i = 0; i < m._rows; i++){
+	for(int j = 0; j < m._cols; j++){
+	    m(i,j) = i+j;
 	}
     }
-    Matrix n(5,3);
-    for(int i = 0 ; i < 5;i++){
-	for(int j = 0; j < 3; j++){
-	    n(i,j) = j*i+(i+j);
-	}
-    }
-    Matrix p = m * n;
-    //m.print();
-    //n.print();
-    //p.print();
+    Matrix k = m;
+    Matrix v = m * k;
+    std::cout<< k << "\n\n";
+    std::cout << m;
+    std::cout << v;
     return 0;
 }
